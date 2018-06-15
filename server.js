ffe0af3c99f84d9a3d28c9c247c589383ad75eb9
t_patrt <tukuna.patro@autodesk.com>
2018-06-15 15:35:10 +0530

    all heroku to assign a port while runing the app; make http server;

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const path = require('path');
 
 const app = express();
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
-let options = {
-  key: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/server.key'),
-  cert: fs.readFileSync((process.env.HOME || process.env.USERPROFILE) + '/.ssh/server.crt')
-};
-let https = require('https').createServer(options, app);
-https.listen(3000, () => console.log('Server running on port 3000'));
+
+let http = require('http').createServer(app);
+const port = process.env.PORT || 3000;
+http.listen(port, () => console.log(`Server running on port ${port}`));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
